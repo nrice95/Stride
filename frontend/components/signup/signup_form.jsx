99f51d3aabc92029efb4099386657a6881b142b5
Nathan Rice <nrice@oberlin.edu>
2018-12-24 22:17:26 -0500

    resolve map directory error

--- frontend/components/signup/signup_form.jsx
+++ frontend/components/signup/signup_form.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 
 class SignupForm extends React.Component {
-  // constructor(props){
+  constructor(props){
     // debugger
     super(props);
     this.state = {
